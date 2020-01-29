545f79dca532f65d5fa9039656023726280ea79a
Saba Zouari <z.saba@gmail.com>
2020-01-29 10:18:38 +0100

    test

--- petSitting_FrontV4/src/main/java/controller/SitterController.java
+++ petSitting_FrontV4/src/main/java/controller/SitterController.java
@@ -1,18 +1,168 @@
 package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+import model.Annonce;
+import repositories.AnnonceRepository;
 
 @Controller
 public class SitterController {
-	@GetMapping("/PostulerAnnonce")
-	public String PostulerAnonncesSitterAffichage () {
-		return "sitter/PostulerAnnonce";	
+	
+	@Autowired
+	AnnonceRepository annonceRepository;
+
+	@GetMapping("sitter/postulerAnnonce") //  a changer a prés par post pour que je puisse voir l erreur par get/ce qu'on ecrit sur le navigateur , c'est l'url appelé sur le front
+	public String AfficherAnnonces(Model model) {
+		List<Annonce> annonces = annonceRepository.selectAllWithStatut0();
+		
+		model.addAttribute("listAnnonces", annonces);
+		return "sitter/postulerAnnonce";// le nom de la jsp
 	}
-	
-	
+
+//	@PostMapping("/postuler")
+//	public String PostulerAnnonce(@ModelAttribute Annonce a,Model model) {
+//		model.addAttribute("annonce", a);
+//		return null;
+//
+//	}
 }
+
+//@Controller
+//public class HelloController {
+//	@PostMapping("/hello") // meme chose qu audessus
+//	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+//	public String methodget() {
+//
+//		return "hello";
+//	}
+//}
+//	@RequestMapping("/bye")
+//	public String bye() {
+//
+//		return "bye";
+//
+//	}
+
+//@PostMapping("/formulaire") //POST MAPPING ici car on veut ENVOYER des infos
+//public String affichage(@ModelAttribute Personne p,Model model) {
+//		model.addAttribute("personne", p);
+//		return "formulaireRecup"; 
+//}
+
+//@RequestMapping(value = "/formulaire", method = RequestMethod.GET)
+//public String formulaireP (@RequestParam(name = "prenom", required = false, defaultValue = "Prenom") String prenom, @RequestParam(name = "nom",required = false,defaultValue ="Nom") String nom,
+//	 Model model) {
+//
+//	model.addAttribute("prenom", prenom);
+//	model.addAttribute("nom", nom);
+//
+//	return "formulaire";
+//	
+//}
+
+//@GetMapping("/list")
+//public String list(Model model) {
+//	//dans le fichier.jsp/c:forEach le nom de l'items doit etre identique a la variable qui est ici ("personnes")
+//	model.addAttribute("personnes", personneRepository.findAll());
+//	return "personne/list"; 
+//}
+
+//	@RequestMapping("/personne")
+//	@Controller
+//	public class PersonneController {
+//		
+//		@Autowired
+//		PersonneRepository personneRepository; 
+//
+//		@Autowired
+//		SalleRepository salleRepository; 
+//		
+//		
+//		
+//		@GetMapping("/list")
+//		public String list(Model model) {
+//			//dans le fichier.jsp/c:forEach le nom de l'items doit etre identique a la variable qui est ici ("personnes")
+//			model.addAttribute("personnes", personneRepository.findAll());
+//			return "personne/list"; 
+//		}
+//		
+//		
+//		@GetMapping("/edit")
+//		public String edit(@RequestParam(name="id") Integer id, Model model) {
+//			Optional<Personne> opt=personneRepository.findById(id); 
+//			Personne p =null; 
+//			if(opt.isPresent()) {
+//				p=opt.get(); 
+//			}
+//			return goEdit(p, model);
+////			model.addAttribute("personne", p);
+////			return "personne/edit"; 
+//		}
+//		
+//		@GetMapping("/addFormateur")
+//		public String addFormateur(Model model) {
+//			return goEdit(new Formateur(), model); 
+//			
+//		}
+//		
+//		@GetMapping("/addEleve")
+//		public String addEleve (Model model) {
+//			return goEdit(new Eleve(), model); 
+//		}
+//		
+//		//pour rassembler les deux editeurs eleve et formateur
+//		public String goEdit(Personne p, Model model) {
+//			model.addAttribute("personne", p); 
+//			model.addAttribute("civilites", Civilite.values());
+//			model.addAttribute("salle", salleRepository.findAll());
+//			
+//			
+//			return "personne/edit"; 
+//		}
+//		
+//		
+//		@PostMapping("/saveEleve")
+//		public String saveEleve(@ModelAttribute("personne")@Valid Eleve personne,BindingResult br, Model model) {
+//			return save (personne,br, model); 
+//		}
+//		
+//		
+//		@PostMapping("/saveFormateur")
+//		//BindingResult faut le mettre juste a^res l on=bjet que vous valider et c est lui qui verifie si il est bien valide nptre objet qui a ete genere par valid si c est bon ou pas pr savoir le truc qui n a pas marche
+//		public String saveFormateur(@ModelAttribute ("personne")@Valid Formateur personne,BindingResult br, Model model) {
+//			return save (personne,br, model); 	
+//		}
+//		
+//		
+//		public String save(Personne personne, BindingResult br, Model model) {
+//			
+//			if (br.hasErrors())
+//				return "personne/edit";
+//			
+//			if (personne.getSalle()!=null&&personne.getSalle().getNom().isEmpty()) {
+//				
+//				personne.setSalle(null);
+//			}
+//			
+//			personneRepository.save(personne); 
+//			return "redirect:/personne/list"; 
+//		}
+//		
+//		
+//		@GetMapping("/delete")
+//		public String delete (@RequestParam(name="id") Integer id) {
+//			personneRepository.deleteById(id);
+//			return "redirect:/personne/list"; 
+//		}
+//		
+//	}
+
 
 
 
---
+++ petSitting_FrontV4/src/main/webapp/WEB-INF/views/sitter/postulerAnnonce.jsp
@@ -0,0 +1,109 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+	crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css"
+	href="${pageContext.request.contextPath}/CSSpetsitting.css">
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+	integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+	crossorigin="anonymous"></script>
+<script
+	src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+	integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+	crossorigin="anonymous"></script>
+<script
+	src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+	integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+	crossorigin="anonymous"></script>
+
+<link rel="icon" type="image/png" href="logo.jpg" />
+
+<meta charset="UTF-8">
+<title>PetSitting</title>
+</head>
+<body>
+	<div id="footer">PETSITTING 2020 AJC Ingénierie - SOPRA</div>
+
+	<div id="entete" class="row">
+
+		<div id="logoEntete" class="col-2">
+			<img height="135" width="130" src="logo.jpg" />
+		</div>
+
+		<div id="titreEntete" class="col-10">
+			<p>
+			<h1>Bienvenue sur PetSitting</h1>
+			</p>
+
+			<p
+				style="font: bold; font-size: 20; text-align: right; margin-right: 5px">
+				Le site de référence pour faire garder vos animaux</p>
+		</div>
+
+	</div>
+
+	<div id="corps" class="row">
+
+		<div id="banderole" class="col-2">
+			<br />
+			<h4 style="font-size: 23; color: white">Menu</h4>
+			<br /> <a href="Sitter consulterAnnoncesS.html"> Consulter mes
+				annonces</a> <br />
+			<br /> <a href="Sitter toutesAnnonces.html">Consulter toutes les
+				annonces </a> <br />
+			<br /> <a href="Sitter postulerAnnonce.html">Postuler à une
+				annonce</a> <br />
+			<br /> <a href="Sitter noterP.html">Noter un propriétaire</a> <br />
+			<br /> <a href="Main page.html">Accueil</a> <br />
+			<br /> <a href="#deco">Me deconnecter </a> <br />
+			<br />
+
+
+		</div>
+
+		<div id="textePrincipal" class="col-8">
+
+			<p>
+			<h2>Pet-sitter > Consulter toutes les annonces</h2>
+			</p>
+
+			<p>
+			<h3>Consulter toutes les annonces</h3>
+			</p>
+			<form method="post" id="afficheForm">
+				<div class="form-group">
+					Voici la liste des annonces existantes :<br> <select>
+						<c:forEach var="annonce" items="${listAnnonces}">
+
+							<option value="${annonce.numA}">${annonce.titre}</option>
+						</c:forEach>
+					</select>
+
+
+				</div>
+				<div class="form-group">
+					<h3>Message</h3>
+					<TEXTAREA id="message" name="message" rows=4 cols=40></TEXTAREA>
+				</div>
+           <input type="submit" name="Submit" value="Postuler" />         
+
+
+			</form>
+
+
+		</div>
+</body>
+</html>
+
+
+
